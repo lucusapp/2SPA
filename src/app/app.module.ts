135e737d80c6d44965a70033f6fde963a8336d20
gavialus <lucusapp@gmail.com>
2017-04-06 22:23:41 +0200

    first deploy

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,41 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+
+
+//rutas
+import { APP_ROUTING } from './app.routes';
+
+//servicios
+import {HeroesService} from './services/heroes.service';
+
+
+//componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    AboutComponent,
+    HeroesComponent,
+    HomeComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
